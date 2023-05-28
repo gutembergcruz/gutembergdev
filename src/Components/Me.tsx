c6e0d213b21528eec5cb950a6de0c409eb311a03
Gutemberg Cruz <gute.tec@gmail.com>
2023-05-28 12:11:10 -0300

    fix about

--- src/Components/Me.tsx
+++ src/Components/Me.tsx
@@ -8,10 +8,15 @@ export function Me() {
       <div className="me-box">
         <div className="me-text">
           <h4 className="text-blue-50 font-bold text-[35px] mb-5">Quem é gutemberg?</h4>
-          <p className="text-white-100 text-[20px] mb-3">Meu nome é Gutemberg do Nascimento Cruz e sou um desenvolvedor web autodidata que atua no mercado de TI desde 2018. Meu objetivo é ajudar empresas a alcançarem o melhor engajamento nos mecanismos de busca, proporcionando a melhor experiência navegável para o usuário final e uma interface clean.</p>
-          <p className="text-white-100 text-[20px] mb-3">Ao longo da minha carreira, tive a oportunidade de participar de diversas maratonas de tecnologias, conhecidas como hackathons, e tive a felicidade de conquistar a vitória em duas ocasiões. Essas experiências me proporcionaram valiosas mentorias e insights no mundo dos negócios.</p>
-          <p className="text-white-100 text-[20px]">Tenho uma paixão por aprender novas habilidades, mesmo que eu não possa me aprofundar e me especializar em tudo. No entanto, mantenho minha curiosidade e dedicação para entender como as coisas funcionam.
-            Quanto à minha comunicação, sou equilibrado, sendo silencioso e focado quando necessário, e dinâmico e comunicativo quando a situação exige.</p>
+          <p className="text-white-100 text-[20px] mb-3">
+          Nascido em Manaus-AM, Gutemberg iniciou sua carreira como Desenvolvedor Front End há 5 anos. Ao longo do tempo, ele refinou suas habilidades e expandiu seu conhecimento como UX/UI Designer e FullStack. Sua trajetória profissional inclui contribuições em projetos para empresas renomadas como Mercedes Benz, Mondelez, Tesla, Itau, Samsung, Nestle, entre outras.
+          </p>
+          <p className="text-white-100 text-[20px] mb-3">
+          Gutemberg sempre buscou desafios e encontrou nos Hackathons o ambiente perfeito para testar suas habilidades. Ele participou de inúmeros eventos desse tipo e, com dedicação e talento, conquistou algumas vitórias notáveis.
+          </p>
+          <p className="text-white-100 text-[20px]">
+          Uma das maiores realizações de sua carreira foi a vitória em seu primeiro hackathon, que lhe rendeu valiosas mentorias presenciais com profissionais de tecnologia no polo tecnológico de Tel Aviv, Israel. Essa experiência proporcionou a Gutemberg uma visão de negócio ampliada, impulsionando-o a buscar sempre a escalabilidade em seus projetos.
+          </p>
         </div>
         <div className="me-img">
           <Image src={meImg} alt="" />
