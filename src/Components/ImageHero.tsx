986aadfff872bf7eb963385692e5c21021c64f51
Gutemberg Cruz <gute.tec@gmail.com>
2023-06-02 10:26:26 -0300

    boost responsive

--- src/Components/ImageHero.tsx
+++ src/Components/ImageHero.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import superHero from '../assets/super.svg'
+import superHeroM from '../assets/superHeroMobile.svg'
 export function ImageHero() {
   return (
     <div className="super-hero">
-      <Image src={superHero} alt="" />
+      <Image className="imageSuperM" src={superHeroM} alt="" />
+      <Image className="imageSuper" src={superHero} alt="" />
     </div>
   )
 }
--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -15,8 +15,8 @@ export function Nav(props: MenuType) {
   const menu = props.menu;
   const [menuMobileActive, setMenuMobileActive] = useState(false);
 
-  function handleActiveMenu(){
-    if(menuMobileActive){
+  function handleActiveMenu() {
+    if (menuMobileActive) {
       setMenuMobileActive(false)
     } else {
       setMenuMobileActive(true)
@@ -24,7 +24,7 @@ export function Nav(props: MenuType) {
   }
   useEffect(() => {
     setMenuMobileActive(false)
-  },[])
+  }, [])
 
   return (
     <header className="flex items-center justify-between py-3 navzada">
@@ -36,9 +36,11 @@ export function Nav(props: MenuType) {
         <Link href="/about" className={menu === 2 ? "item-menu item-menu-active" : "item-menu"}>Sobre mim</Link>
         <Link href="/cases" className={menu === 3 ? "item-menu item-menu-active" : "item-menu"}>Cases</Link>
         <button className="item-menu" onClick={() => window.scrollTo(0, document.body.scrollHeight)}>Contato</button>
-        <Link href="https://drive.google.com/file/d/1EM9rV_RSB9qq0COhONkNTXJ5aAfHOgrZ/view?usp=share_link" target="_blank" rel="noopener noreferrer" className="item-menu-on bg-blue-150 hover:bg-blue-50 px-5 flex text-white-100 py-2 rounded-lg gap-3 items-center justify-center">Download CV <FiDownload /> </Link>
+        <Link href="https://drive.google.com/file/d/1IA5YSSkaK3HVKam_qw7wJRzR022gsCqW/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="item-menu-on bg-blue-150 hover:bg-blue-50 px-5 flex text-white-100 py-2 rounded-lg gap-3 items-center justify-center">Download CV <FiDownload /> </Link>
         <button className="item-menu-off" onClick={() => handleActiveMenu()}>
-          <FiMenu />
+          <p>
+            <FiMenu />
+          </p>
         </button>
       </nav>
       <div className={menuMobileActive ? "menu-mobile menu-mobile-actived" : "menu-mobile"}>
@@ -46,8 +48,8 @@ export function Nav(props: MenuType) {
         <Link href="/" className={menu === 1 ? "item-menu-active" : ""}>Inicio</Link>
         <Link href="/about" className={menu === 2 ? "item-menu-active" : ""}>Sobre mim</Link>
         <Link href="/cases" className={menu === 3 ? "item-menu-active" : ""}>Cases</Link>
-        <button className="" onClick={() => { handleActiveMenu(); window.scrollTo(0, document.body.scrollHeight)}}>Contato</button>
-        <Link href="https://drive.google.com/file/d/1EM9rV_RSB9qq0COhONkNTXJ5aAfHOgrZ/view?usp=share_link" target="_blank" rel="noopener noreferrer" className="bg-red-150 hover:bg-red-50 px-5 flex text-white-100 py-2 rounded-lg gap-3 items-center justify-center">Download CV <FiDownload /> </Link>
+        <button className="" onClick={() => { handleActiveMenu(); window.scrollTo(0, document.body.scrollHeight) }}>Contato</button>
+        <Link href="https://drive.google.com/file/d/1IA5YSSkaK3HVKam_qw7wJRzR022gsCqW/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="bg-red-150 hover:bg-red-50 px-5 flex text-white-100 py-2 rounded-lg gap-3 items-center justify-center">Download CV <FiDownload /> </Link>
       </div>
     </header>
   )
