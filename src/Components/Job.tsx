ae6bbc7ea2980a987cdf71d392219590cde455c9
Gutemberg Cruz <gutemberg.cruz@profranchising.com.br>
2024-07-08 22:14:24 -0300

    fix description

--- src/Components/Job.tsx
+++ src/Components/Job.tsx
@@ -10,7 +10,7 @@ export function Job() {
         <Title text="Meu trampo" />
         <Image className="losango-img" src={losangoImg} alt="" />
         <p className="text-white-100">
-        Com uma sólida experiência de 5 anos no mercado de TI, meu principal objetivo é auxiliar <br /> <span>sua empresa</span> a alcançar um alto nível de engajamento nos <span>mecanismos de busca</span>, <br />proporcionando uma <span>experiência</span> de navegação otimizada para o usuário final e uma <br /> interface <span>limpa e intuitiva</span>.
+        Com uma sólida experiência de 6 anos no mercado de TI, meu principal objetivo é auxiliar <br /> <span>sua empresa</span> a alcançar um alto nível de engajamento nos <span>mecanismos de busca</span>, <br />proporcionando uma <span>experiência</span> de navegação otimizada para o usuário final e uma <br /> interface <span>limpa e intuitiva</span>.
       </p>
       </div>
       
